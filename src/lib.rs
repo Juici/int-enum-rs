63950a130c2c0dab7711ce77ab90bcb35fc34a5a
James Whaley <juicy66173@gmail.com>
2024-01-20 18:33:32 +0000

    Fix issues from clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate proc_macro;
 
 mod ast;
 mod expand;
-mod ext;
+mod span_ext;
 
 use proc_macro::TokenStream;
 use proc_macro2_diagnostics::Diagnostic;
@@ -36,5 +36,5 @@ pub(crate) type Result<T, E = Diagnostic> = std::result::Result<T, E>;
 #[proc_macro_derive(IntEnum)]
 pub fn int_enum_derive(input: proc_macro::TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    expand::derive(input).unwrap_or_else(|d| d.emit_as_item_tokens()).into()
+    expand::derive(input).unwrap_or_else(Diagnostic::emit_as_item_tokens).into()
 }
