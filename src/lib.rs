e48f1b88db3e46b67ac0d967ce3bb5705ce2d60f
James Whaley <juicy66173@gmail.com>
2024-01-20 18:14:49 +0000

    Add docs to macro

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,27 @@ use syn::{parse_macro_input, DeriveInput};
 
 pub(crate) type Result<T, E = Diagnostic> = std::result::Result<T, E>;
 
+/// Derive macro for conversion between integer and enum.
+///
+/// # Example
+///
+/// ```
+/// use int_enum::IntEnum;
+///
+/// #[repr(u8)]
+/// #[derive(Debug, PartialEq, IntEnum)]
+/// pub enum Ascii {
+///     UpperA = b'A',
+///     UpperB = b'B',
+/// }
+///
+/// assert_eq!(u8::from(Ascii::UpperA), b'A');
+/// assert_eq!(u8::from(Ascii::UpperB), b'B');
+///
+/// assert_eq!(Ascii::try_from(b'A'), Ok(Ascii::UpperA));
+/// assert_eq!(Ascii::try_from(b'B'), Ok(Ascii::UpperB));
+/// assert_eq!(Ascii::try_from(b'C'), Err(b'C'));
+/// ```
 #[proc_macro_derive(IntEnum)]
 pub fn int_enum_derive(input: proc_macro::TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
